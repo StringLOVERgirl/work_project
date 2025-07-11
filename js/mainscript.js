77fb9db056bcefe1c03043483d067f1a4dbe4b8c
StringLOVERgirl <marchenkostar@gmail.com>
2025-07-11 07:30:06 +0300

    canvas has been added and mainscript updated

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,12 +1,19 @@
 import {cursor} from './cursor.js'
 import {parallax} from './parallax.js'
 import { animateHeader } from './header.js';
+import {  init3d} from './ThreeLearning.js';
 
 document.addEventListener('DOMContentLoaded',startJs)
 
 function startJs(){
+    let cont = document.querySelector('.cont')
+
+
     animateHeader()
     document.addEventListener('mousemove', cursor);
     window.addEventListener('scroll',parallax);    
+    init3d(cont)
+    console.log(cont)
 }
+
 
